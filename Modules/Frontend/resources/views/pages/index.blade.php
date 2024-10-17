41326a54afe91852a31c793c156c589651885a5a
jauharimtikhan <jauhar.imtikhan@gmail.com>
2024-10-17 17:43:30 +0700

    Revert "tambah conten4"
    This reverts commit 2cff5fb02544ae9cd7c79d046f07a3cb21c72d12.

--- Modules/Frontend/resources/views/pages/index.blade.php
+++ Modules/Frontend/resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
     {{-- Hero Start --}}
     <section class="bg-white md:py-16 max-w-screen-xl antialiased dark:bg-gray-900  mx-auto">
-        <div class="z-[10] relative dark:bg-gray-900 pb-8 px-6 rounded-lg">
+        <div class="z-[10] relative dark:bg-gray-900 pb-8 px-6 rounded-lg" data-aos="fade-down">
             <div id="controls-carousel" class="relative w-full  dark:bg-gray-900" data-carousel="slide">
                 <!-- Carousel wrapper -->
                 <div class="relative h-56 md:h-[30rem] overflow-hidden dark:bg-gray-900 rounded-lg ">
@@ -148,22 +148,36 @@
 
     <section class="bg-white-50 py-8 antialiased dark:bg-gray-900 md:py-16">
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
-            <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
-                <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Kategori</h2>
+            <h2 class="mt-3 mb-3 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl" data-aos="fade-up"
+                data-aos-duration="3000">Kategori
+            </h2>
+            <!-- Heading & Filters -->
 
-
-            </div>
-
-            <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                @foreach ($categories as $category)
-                    <a href="#"
-                        class="flex items-center md:justify-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-
-                        <span class="text-sm font-medium text-gray-900 dark:text-white">{{ $category?->name }}</span>
+            <div class="mb-4 grid gap-4 grid-cols-3 md:mb-8 lg:grid-cols-3 xl:grid-cols-7 md:grid-cols-2"
+                data-aos="fade-up" data-aos-duration="3000">
+                <div class="h-40 w-full">
+                    <a href="#">
+                        <img class="mx-auto h-full dark:hidden " src="{{ asset('frontend/img/jaket.png') }}"
+                            alt="" />
+                        <img class="mx-auto hidden h-full dark:block" src= "{{ asset('frontend/img/jaket.png') }}"
+                            alt="" />
                     </a>
-                @endforeach
+                    </class=>
+
+
+                </div>
+
+                <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    @foreach ($categories as $category)
+                        <a href="#"
+                            class="flex items-center md:justify-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+
+                            <span
+                                class="text-sm font-medium text-gray-900 dark:text-white">{{ $category?->name }}</span>
+                        </a>
+                    @endforeach
+                </div>
             </div>
-        </div>
     </section>
 
     {{-- Featured Start --}}
@@ -213,7 +227,8 @@
                     Save $500 today on your purchase <br />
                     of a new iMac computer.
                 </h1>
-                <p class="mb-6 text-gray-500 dark:text-gray-400">Reserve your new Apple iMac 27” today and enjoy
+                <p class="mb-6 text-gray-500 dark:text-gray-400">Reserve your new Apple iMac 27” today and
+                    enjoy
                     exclusive savings with qualified activation. Pre-order now to secure your discount.</p>
                 <a href="#"
                     class="inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
